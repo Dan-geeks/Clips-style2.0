68e7b864551b92e8a12ac11761f5a697e834382d
unknown <mpe009@uoeld.ac.ke>
2025-04-11 03:15:14 +0300

    This nearly killed me

--- android/app/src/main/kotlin/com/example/clipsandstyles2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/clipsandstyles2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.clipsandstyles2
+package com.example.clipsandstyles3
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
